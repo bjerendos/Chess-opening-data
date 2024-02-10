dab7c43db998858b873a2943abd9ebdf69c8fafc
bjerendos <159655139+bjerendos@users.noreply.github.com>
2024-02-10 22:24:47 +0100

    Update Lichess opening data processing-User friendly.py

--- Lichess opening data processing-User friendly.py
+++ Lichess opening data processing-User friendly.py
@@ -159,6 +159,7 @@ SOURCES AND COPYRIGHT INFORMATION:
     - Script written and published by Berend van Hapert, February 2024  
 
 LICENSE:
+
     - This project is licensed under the MIT License. See the LICENSE file for details.
      
 '''
